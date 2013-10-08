f26b465e960dbbe506ffd11d9b8ed556d6db9f86
Jeff Tsui <tsui.jeff@gmail.com>
2013-10-08 00:14:24 -0700

    add chem tagging

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -6,17 +6,22 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import nu.xom.Document;
+
 import org.codehaus.jackson.JsonNode;
 import org.codehaus.jackson.JsonParseException;
 import org.codehaus.jackson.map.JsonMappingException;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.codehaus.jackson.type.TypeReference;
+import org.json.JSONException;
 import org.json.JSONObject;
+import org.json.XML;
 
 import play.libs.Json;
 import play.mvc.BodyParser;
 import play.mvc.Controller;
 import play.mvc.Result;
+import uk.ac.cam.ch.wwmm.chemicaltagger.Utils;
 import act.server.Molecules.BRO;
 import act.server.Molecules.CRO;
 import act.server.Molecules.DotNotation;
@@ -44,14 +49,24 @@ public class Application extends Controller {
 	public static IndigoInchi indigoInchi = null;
 
 	public static Result index() {
-		String title = "Act REST API";
-		String apply = "To apply an RO send GET request to /apply with the following JSON parameters:";
+		String title = "============\nACT REST API\n============";
+		String apply = "APPLY RO\n----------------------\nTo apply an RO send GET request to /apply with the following JSON parameters:";
 		String applyParams = "{\"substrate\": <your substrate in smiles>, \"ro_type\": <ERO or CRO>, \"rxn_id\": <the representative reaction id of the ro_type>}";
-		String applyEx = "Example: curl --header \"Content-type: application/json\" --request GET --data '{\"rxn_id\":\"1\",\"ro_type\":\"CRO\",\"substrate\":\"[C@@H]12[C@@H](O1)[C@@H](C=C(C2=O)CO)O\"}' http://localhost:9000/apply";
-		String infer = "To infer an ERO send GET request to /infer with the following JSON parameters:";
+		String applyEx = "Example input:\ncurl --header \"Content-type: application/json\" --request GET --data '{\"rxn_id\":\"1\",\"ro_type\":\"CRO\",\"substrate\":\"[C@@H]12[C@@H](O1)[C@@H](C=C(C2=O)CO)O\"}' http://localhost:9000/apply";
+		String applyExOut = "Example output:\n{\"reverse\":[[\"[H]C([H])(O[H])C1=C([H])C([H])(O[H])C2([H])OC2([H])C1([H])O[H]\"]],\"forward\":[[\"OC1C(=C([H])C(=O)C2([H])OC21[H])C([H])([H])O[H]\"],[\"O=C([H])C1C([H])C([H])(O[H])C2([H])OC2([H])C1=O\"]]}";
+		String infer = "INFER ERO\n----------------------\nTo infer an ERO send GET request to /infer with the following JSON parameters:";
 		String inferParams = "{\"substrates\": [<list of your substrate in smiles>], \"products\": [<list of your products in smiles>]}";
-		return ok(title + "\n\n" + apply + "\n" + applyParams + "\n" + applyEx
-				+ "\n\n" + infer + "\n" + inferParams);
+		String inferEx = "Example input:\ncurl --header \"Content-type: application/json\" --request GET --data '{\"substrates\": [\"[C@@H]23[C@H]([C@H]1[C@]([C@@H](C(C)=O)CC1)(C)CC2)CCC4=CC(=O)CC[C@]34C\"], \"products\": [\"[C@H]34[C@H]2[C@@H]([C@@]1(C(=CC(=O)CC1)CC2)C)CC[C@@]3([C@@H](C(CO)=O)CC4)C\"]}' http://localhost:9000/infer";
+		String inferExOut = "Example output:\n{\"BRO\":\"{C-O=1, C-H=-1, H-O=1}\",\"CRO\":\"{ [H,*:1]O[H].[H,*:2]C([H,*:3])([H])[H,*:4][Ac]>>[H,*:1]OC([H,*:2])([H,*:3])[H,*:4][Ac] }\",\"ERO\":\"{ [H,*:1]C([H,*:2])([H,*:3])C([Ac])(O[Ac])C([H])([H])[H]>>[H,*:1]C([H,*:2])([H,*:3])C([Ac])(O[Ac])C([H])([H])O[H] }\"}";
+		String tag = "TAG CHEMICALS\n----------------------\nTo tag the chemicals in text.";
+		String tagParams = "{\"paper\": <any chunk of text>}";
+		String tagEx = "curl --header \"Content-type: application/json\" --request GET --data '{\"paper\": \"Reduction of 5-methoxy-6-formyl(Ia)- and 5-formyl-6-methoxy-2,3-diphenylbenzofuran (IVa) yielded 6- and 5-methyl derivatives Ib and IVb, respectively.\"}' http://localhost:9000/tag";
+		String tagExOut = "{\"Document\":{\"Sentence\":{\"Unmatched\":{\"RB\":\"respectively\"},\"STOP\":\".\",\"COMMA\":\",\",\"ActionPhrase\":{\"NounPhrase\":[{\"PrepPhrase\":{\"NounPhrase\":{\"MOLECULE\":[{\"OSCARCM\":{\"OSCAR-CM\":\"5-methoxy-6-formyl(Ia)-\"}},{\"OSCARCM\":[{\"OSCAR-CM\":\"5-formyl-6-methoxy-2,3-diphenylbenzofuran\"},{\"_-RRB-\":\")\",\"OSCAR-CM\":\"IVa\",\"_-LRB-\":\"(\"}]}],\"CC\":\"and\"},\"IN-OF\":\"of\"},\"NN\":\"Reduction\"},{\"NNS\":\"derivatives\",\"MOLECULE\":[{\"OSCARCM\":{\"OSCAR-CM\":\"5-methyl\"}},{\"OSCARCM\":{\"OSCAR-CM\":\"Ib\"}},{\"OSCARCM\":{\"OSCAR-CM\":\"IVb\"}}],\"CC\":[\"and\",\"and\"],\"CD\":\"6-\"}],\"type\":\"Yield\",\"VerbPhrase\":{\"VB-YIELD\":\"yielded\"}}}}}";
+		return ok(title + "\n\n\n" + apply + "\n" + applyParams + "\n\n"
+				+ applyEx + "\n\n" + applyExOut + "\n\n\n" + infer + "\n"
+				+ inferParams + "\n\n" + inferEx + "\n\n" + inferExOut
+				+ "\n\n\n" + tag + "\n" + tagParams + "\n\n" + tagEx + "\n\n"
+				+ tagExOut);
 	}
 
 	// This function will be used by all server side function to initiate
@@ -128,6 +143,28 @@ public class Application extends Controller {
 		result.put("CRO", theoryRO.CRO().toString());
 		result.put("BRO", theoryRO.BRO().toString());
 		return ok(Json.toJson(result));
+	}
+
+	@BodyParser.Of(BodyParser.Json.class)
+	public static Result tag() {
+		JsonNode json = request().body().asJson();
+		System.out.println(json);
+		String abstractText = json.findPath("paper").getTextValue();
+		if (abstractText == null) {
+			return badRequest("Missing parameter [paper]");
+		}
+		String xmltag = null;
+		JSONObject item = null;
+		try {
+			Document doc = Utils.runChemicalTagger(abstractText);
+			xmltag = doc.toXML();
+			item = XML.toJSONObject(xmltag);
+			return ok(Json.parse(item.toString()));
+		} catch (JSONException e) {
+			return badRequest("Error parsing " + e);
+		} catch (Exception e1) {
+			return badRequest("Error parsing " + e1);
+		}
 	}
 
 	@BodyParser.Of(BodyParser.Json.class)
